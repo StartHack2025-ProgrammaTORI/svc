d233439710727a3584cb4dbe6d233fca42257096
Michele Mattiello <michele.mattiello2001@gmail.com>
2025-03-21 00:53:12 +0100

    fix: response

--- svc/plugin/proposal/repository.py
+++ svc/plugin/proposal/repository.py
@@ -1,12 +1,51 @@
 from svc.utils.database import db
-from .schema import InputProposal, Proposal
+from .schema import InputProposal, ProposalPopulated
 from bson import ObjectId  # Import ObjectId for querying by ID
 
 def create_proposal(proposal: InputProposal):
     db.proposal.insert_one(proposal.dict())
 
 def find_proposals(company_id: str):
-    return [Proposal(**p) for p in db.proposal.find({'consultancy_receiver': company_id})]
+    pipeline = [
+        {'$match': {'consultancy_receiver': company_id}},
+        {
+            '$addFields': {
+                'consultancy_receiver_obj': {'$toObjectId': '$consultancy_receiver'},
+                'consultancy_provider_obj': {'$toObjectId': '$consultancy_provider'}
+            }
+        },
+        {
+            '$lookup': {
+                'from': 'company',
+                'localField': 'consultancy_receiver_obj',
+                'foreignField': '_id',
+                'as': 'consultancy_receiver'
+            }
+        },
+        {'$unwind': '$consultancy_receiver'},
+        {
+            '$lookup': {
+                'from': 'company',
+                'localField': 'consultancy_provider_obj',
+                'foreignField': '_id',
+                'as': 'consultancy_provider'
+            }
+        },
+        {'$unwind': '$consultancy_provider'},
+        {
+            '$project': {
+                "_id": 1,
+                "consultancy_receiver": 1,
+                "consultancy_provider": 1,
+                "status": 1,
+                "reason_of_rejection": 1,
+                "reason_of_match": 1,
+                "created_at": 1
+            }
+        }
+    ]
+    data = db.proposal.aggregate(pipeline)
+    return [ProposalPopulated(**p) for p in data]
 
 def update_proposal_status(proposal_id: str, status: str):
     db.proposal.update_one(
--- svc/plugin/proposal/schema.py
+++ svc/plugin/proposal/schema.py
@@ -4,6 +4,7 @@ from pydantic_mongo import PydanticObjectId
 from pydantic import Field
 from datetime import datetime
 from enum import Enum
+from svc.plugin.consultant.schema import Consultant
 
 class Status(str, Enum):
     PENDING = "PENDING"
@@ -22,6 +23,15 @@ class Proposal(BaseModel):
     id: PydanticObjectId = Field(alias="_id")
     consultancy_receiver: str # company id
     consultancy_provider: str # company id
+    status: Status = Status.PENDING
+    reason_of_rejection: Optional[str] = None
+    reason_of_match: Optional[str] = None
+    created_at: datetime = datetime.now()
+    
+class ProposalPopulated(BaseModel):
+    id: PydanticObjectId = Field(alias="_id")
+    consultancy_receiver: Consultant
+    consultancy_provider: Consultant
     status: Status = Status.PENDING
     reason_of_rejection: Optional[str] = None
     reason_of_match: Optional[str] = None
