01ee2c76255a6dd5a296a7efc2c352385514dd39
Michele Mattiello <michele.mattiello2001@gmail.com>
2025-03-21 00:30:12 +0100

    feat

--- svc/plugin/proposal/controller.py
+++ svc/plugin/proposal/controller.py
@@ -5,9 +5,20 @@ from svc.utils.dataset import answers
 from svc.plugin.question import repository as question_repository
 from svc.plugin.consultant import repository as consultant_repository
 from . import repository as repository_proposal
-from .schema import InputProposal, Proposal
+from .schema import InputProposal
 
 router = APIRouter(prefix="/proposals", tags=["proposals"])
+
+@router.post("/{id}")
+async def update_proposal(
+    id: str,
+    body: dict,
+    user: dict = Depends(validate_token)
+    ):
+    db_user = consultant_repository.get_user(user['uid'])
+    repository_proposal.update_proposal_status(id, body['status'])
+    proposals = repository_proposal.find_proposals(str(db_user['company']))
+    return {"message": "Consultant created successfully", "data": proposals }
 
 @router.get("")
 async def get_proposals(user: dict = Depends(validate_token)):
@@ -15,7 +26,7 @@ async def get_proposals(user: dict = Depends(validate_token)):
     my_company = consultant_repository.get_consultant(db_user['company'])
     if my_company == None:
         return {"message": "Consultant not found", "data": [] }
-    proposals = repository_proposal.find_proposals(user['uid'])
+    proposals = repository_proposal.find_proposals(str(db_user['company']))
     if len(proposals) == 0:
         questions = question_repository.get_questions_and_answers(user['uid'])
         recomendations = userRec.find_best_match(questions)
--- svc/plugin/proposal/repository.py
+++ svc/plugin/proposal/repository.py
@@ -1,8 +1,15 @@
 from svc.utils.database import db
 from .schema import InputProposal, Proposal
+from bson import ObjectId  # Import ObjectId for querying by ID
 
 def create_proposal(proposal: InputProposal):
     db.proposal.insert_one(proposal.dict())
 
 def find_proposals(company_id: str):
     return [Proposal(**p) for p in db.proposal.find({'consultancy_receiver': company_id})]
+
+def update_proposal_status(proposal_id: str, status: str):
+    db.proposal.update_one(
+        {'_id': ObjectId(proposal_id)}, 
+        {'$set': {'status': status}}
+    )
