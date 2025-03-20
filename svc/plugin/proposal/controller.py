51a0a38972fda4e526eb24d10eac854d4ef70d74
Michele Mattiello <michele.mattiello2001@gmail.com>
2025-03-20 23:57:03 +0100

    feat: proposals

--- svc/plugin/proposal/controller.py
+++ svc/plugin/proposal/controller.py
@@ -12,21 +12,20 @@ router = APIRouter(prefix="/proposals", tags=["proposals"])
 @router.get("")
 async def get_proposals(user: dict = Depends(validate_token)):
     db_user = consultant_repository.get_user(user['uid'])
-    print("user: ", str(db_user['company']))
     my_company = consultant_repository.get_consultant(db_user['company'])
-    print(my_company)
+    if my_company == None:
+        return {"message": "Consultant not found", "data": [] }
     proposals = repository_proposal.find_proposals(user['uid'])
     if len(proposals) == 0:
         questions = question_repository.get_questions_and_answers(user['uid'])
         recomendations = userRec.find_best_match(questions)
-        for recomendation in recomendations:
-            print("test: ", my_company, recomendation)
+        for recomendation in recomendations['consultants']:
             repository_proposal.create_proposal(
                 InputProposal(
-                    consultancy_receiver=my_company['_id'],
-                    consultancy_provider=recomendation['_id'],
+                    consultancy_receiver=str(my_company['_id']),
+                    consultancy_provider=str(recomendation['_id']),
                     reason_of_match=recomendation['reason']
                 )
             )
-    proposals = repository_proposal.find_proposals(user['uid'])
+    proposals = repository_proposal.find_proposals(str(db_user['company']))
     return {"message": "Consultant created successfully", "data": proposals }
