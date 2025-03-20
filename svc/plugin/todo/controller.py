c1e4c2c9e0c04f3f7f52f7705c269f47ecc954ae
Michele Mattiello <michele.mattiello2001@gmail.com>
2025-03-20 14:23:08 +0100

    feat: check todo

--- svc/plugin/todo/controller.py
+++ svc/plugin/todo/controller.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
 from . import repository
+
 router = APIRouter(prefix="/todos", tags=["todos"])
 
 @router.get("")
 async def get_todos():
     return { "data": repository.get_todos() }
+
+@router.post("/{id}")
+async def check_todos(id: int):
+    return { "data": repository.check_todo(id) }
--- svc/plugin/todo/repository.py
+++ svc/plugin/todo/repository.py
@@ -1,4 +1,14 @@
 from svc.utils.dataset import todos
 
 def get_todos():
-    return todos
+    result = []
+    for todo in todos:
+        if todo['current_step'] < len(todo['steps']):
+            result.append(todo)
+    return result
+
+def check_todo(id: int):
+    for index, todo in enumerate(todos):
+        if todo['id'] == id:
+            todos[index]['current_step'] = todos[index]['current_step'] + 1
+    return get_todos()
